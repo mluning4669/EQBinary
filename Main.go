9fb373121bccd1ba2db2eeeaaf1f6e7b12768e36
Mike <mluning4669@gmail.com>
2020-01-20 16:36:47 -0600

    Walk works

--- Main.go
+++ Main.go
@@ -9,15 +9,35 @@ import (
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
 func Walk(t *tree.Tree, ch chan int) {
-	fmt.Println("placeholder")
+
+	if t.Left != nil {
+		Walk(t.Left, ch)
+	}
+
+	ch <- t.Value
+
+	if t.Right != nil {
+		Walk(t.Right, ch)
+	}
+
 }
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
+// Same(tree.New(1),tree.New(1)) should return true
+// Same(tree.New(1),tree.New(2)) should return false
+// Possibly use a switch statement
 func Same(t1, t2 *tree.Tree) bool {
 	return true
 }
 
 func main() {
 	fmt.Println("hello!")
+	t1 := tree.New(1)
+	ch := make(chan int)
+
+	go Walk(t1, ch)
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-ch)
+	}
 }
