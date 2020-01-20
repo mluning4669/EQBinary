b09ac9552b55d8da26f8a09e0dab4ef08627adf1
Mike <mluning4669@gmail.com>
2020-01-20 16:45:34 -0600

    implement basic Same function

--- Main.go
+++ Main.go
@@ -28,16 +28,26 @@ func Walk(t *tree.Tree, ch chan int) {
 // Same(tree.New(1),tree.New(2)) should return false
 // Possibly use a switch statement
 func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for i := 0; i < 10; i++ {
+		if <-ch1 != <-ch2 {
+			return false
+		}
+	}
 	return true
 }
 
 func main() {
-	fmt.Println("hello!")
 	t1 := tree.New(1)
-	ch := make(chan int)
+	t2 := tree.New(2)
 
-	go Walk(t1, ch)
-	for i := 0; i < 10; i++ {
-		fmt.Println(<-ch)
+	if Same(t1, t2) {
+		fmt.Println("t1 and t2 same")
+	} else {
+		fmt.Println("t1 and t2 different")
 	}
 }
