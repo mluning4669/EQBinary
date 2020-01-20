2cc51c05efadd925fe87d396df6c19dc98cefb93
Mike <mluning4669@gmail.com>
2020-01-20 11:35:55 -0600

    Install packages and get Main to a runnable state

---
+++ Main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/tour/tree"
+)
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	fmt.Println("placeholder")
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	return true
+}
+
+func main() {
+	fmt.Println("hello!")
+}
